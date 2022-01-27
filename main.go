b10cbf4ee5d274ecc4dec872d0e61bc45bca3518
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-27 20:47:44 +0900

    Tickの取得

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"go-trading/bitflyer"
 	"go-trading/config"
 	"go-trading/utils"
+	"time"
 )
 
 func main() {
 	utils.LoggingSetting(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	fmt.Println(apiClient.GetBalance())
+	ticker, _ := apiClient.GetTicker("BTC_USD")
+	fmt.Println(ticker)
+	fmt.Println(ticker.GetMidPrice())
+	fmt.Println(ticker.DateTime())
+	fmt.Println(ticker.TruncateDateTime(time.Hour))
 }
