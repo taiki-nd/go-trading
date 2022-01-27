9478b1eb5c1c52c8b382c7a1f076d30c47a49aa6
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-27 17:55:20 +0900

    ヘッダーを使ったリクエスト

--- bitflyer/bitflyer.go
+++ bitflyer/bitflyer.go
@@ -1,9 +1,11 @@
 package bitflyer
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -82,5 +84,39 @@ func (api APIClient) header(method, endpoint string, body []byte) map[string]str
 	}
 }
 
+func (api *APIClient) doRequest(method, urlPath string, query map[string]string, data []byte) (body []byte, err error) {
+	baseURL, err := url.Parse(baseURL) //正しいURLか確認
+	if err != nil {
+		return
+	}
+	apiURL, err := url.Parse(urlPath)
+	if err != nil {
+		return
+	}
+	endpoint := baseURL.ResolveReference(apiURL).String()
+	log.Panicf("action=doRequest endpoint=%s", endpoint)
+	req, err := http.NewRequest(method, endpoint, bytes.NewBuffer(data))
+	if err != nil {
+		return
+	}
+	q := req.URL.Query()
+	for key, value := range query {
+		q.Add(key, value)
+	}
+	req.URL.RawQuery = q.Encode()
 
+	for key, value := range api.header(method, req.URL.RequestURI(), data) {
+		req.Header.Add(key, value)
+	}
+
+	resp, err := api.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
