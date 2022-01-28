f0c528b423f9dcd95c90739457551a2110688fd2
Taiki Noda <taiki.nd.real@gmail.com>
2022-01-29 01:31:53 +0900

    データベースの作成

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.17
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.10 // indirect
 	gopkg.in/ini.v1 v1.66.3 // indirect
 )
--- main.go
+++ main.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"fmt"
-	"go-trading/bitflyer"
+	"go-trading/app/models"
 	"go-trading/config"
 	"go-trading/utils"
-	"time"
 )
 
 func main() {
 	utils.LoggingSetting(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	/*
+		apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 
-	tickerChannel := make(chan bitflyer.Ticker)
-	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
-	for ticker := range tickerChannel {
-		fmt.Println(ticker)
-		fmt.Println(ticker.GetMidPrice())
-		fmt.Println(ticker.DateTime())
-		fmt.Println(ticker.TruncateDateTime(time.Second))
-		fmt.Println(ticker.TruncateDateTime(time.Minute))
-		fmt.Println(ticker.TruncateDateTime(time.Hour))
-	}
+		tickerChannel := make(chan bitflyer.Ticker)
+		go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
+		for ticker := range tickerChannel {
+			fmt.Println(ticker)
+			fmt.Println(ticker.GetMidPrice())
+			fmt.Println(ticker.DateTime())
+			fmt.Println(ticker.TruncateDateTime(time.Second))
+			fmt.Println(ticker.TruncateDateTime(time.Minute))
+			fmt.Println(ticker.TruncateDateTime(time.Hour))
+		}
+	*/
+	fmt.Println(models.DbConnection)
 }
